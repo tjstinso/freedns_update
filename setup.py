20145b7e7e9cf6fd9fc7e591d14f99d1e7285dd5
tyler stinson <tjstinso@gmail.com>
2016-11-29 08:37:18 +0000

    need to edit setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
-
-setup(name='Tyler Stinson',
-    version='1.0',
-    description='script to update ip on freedns.afraid.org',
-	author='Tyler Stinson',
- 	author_email='tjstinso@gmail.com',
-    url='',
-    packages=['ipgetter'],
-)
+#need to rewrite this part
+#from distutils.core import setup
+#
+#setup(name='Tyler Stinson',
+#    version='1.0',
+#    description='script to update ip on freedns.afraid.org',
+#	author='Tyler Stinson',
+# 	author_email='tjstinso@gmail.com',
+#    url='',
+#)
